b155a2a34332e4e2615a563e1f36ee017d263c2b
Rajkumar Ojha <rajkumarojha532@gmail.com>
2025-05-29 20:27:43 +0530

    responsive update

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
+      <div>
         <Navbar />
-        <div className="flex-grow pt-16">
+        <div className=" pt-16">
           <SpecialRoutes />
         </div>
         <Footer />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -118,7 +118,7 @@ const legalServices = [
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
+    <div className=" max-w-full overflow-hidden">
       {/* --- Hero Section --- */}
       <section className="relative min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-900 via-blue-800 to-indigo-900 overflow-x-hidden ">
 
