cc2409f14ec498a32cb5796ec09a0606d0eb32dc
Rajkumar Ojha <rajkumarojha532@gmail.com>
2025-06-06 16:11:11 +0530

    fix: correct import names for SignUp and SignIn components in SpecialRoutes

--- src/routes/SpecialRoutes.jsx
+++ src/routes/SpecialRoutes.jsx
@@ -3,8 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import FindLawyer from '../pages/FindLawyer';
 import BlogPage from '../pages/BlogPage';
-import Signup from '../pages/Signup';
-import Signin from '../pages/Signin';
+import SignUp from '../pages/SignUp';
+import SignIn from '../pages/SignIn';
 import ConfirmationPage from '../pages/ConfirmationPage';
 import BookingPage from "../pages/BookingPage"
 import PaymentPage from "../pages/PaymentPage";
@@ -17,8 +17,8 @@ const SpecialRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/find-lawyer" element={<FindLawyer />} />
       <Route path="/blog" element={<BlogPage />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
        <Route path="/book/:lawyerId" element={<BookingPage />} />
        <Route path="/book/:lawyerId/payment" element={<PaymentPage />} />
       <Route path="/book/:lawyerId/confirmation" element={<ConfirmationPage />} />
